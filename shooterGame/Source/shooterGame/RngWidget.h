420d2d63535c1e17a1fea008fe0f3d4541b059f3
Harriet Lee <lharriet27@gmail.com>
2021-04-27 22:45:34 +0900

    feat : add widget class

---
+++ shooterGame/Source/shooterGame/RngWidget.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RngWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SHOOTERGAME_API URngWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* RandomLable;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* button;
+
+};
