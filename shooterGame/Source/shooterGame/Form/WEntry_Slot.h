67eff621562a71244f28d3bae3c92d0769befd71
Harriet Lee <lharriet27@gmail.com>
2021-05-11 23:24:28 +0900

    feat: add new widget and classes -noticePopup

---
+++ shooterGame/Source/shooterGame/Form/WEntry_Slot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Form/WFormBase.h"
+#include "WEntry_Slot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UWEntry_Slot : public UWFormBase
+{
+	GENERATED_BODY()
+	
+};
